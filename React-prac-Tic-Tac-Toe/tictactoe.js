0a4211b0727a7b9d23a0f00cbcbe408ca6e8bdab
KasaHNO3 <umbrella.hsiao@gmail.com>
2020-11-03 20:08:02 +0800

    Update tictactoe.js

--- React-prac-Tic-Tac-Toe/tictactoe.js
+++ React-prac-Tic-Tac-Toe/tictactoe.js
@@ -31,10 +31,7 @@ class Square extends React.Component {
   }
   render() {
     return (
-      <button
-        className="square"
-        onClick= {()=> this.setState({value:'X'})}
-      >        
+      <button className="square" onClick= {()=> this.setState({value:'X'})}>        
         {this.state.value}
       </button>
     );
