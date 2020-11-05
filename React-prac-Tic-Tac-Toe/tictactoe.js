5f3c7f0ea4db09b632888ff96c4157df00fdf6df
KasaHNO3 <umbrella.hsiao@gmail.com>
2020-11-05 12:57:15 +0800

    Update tictactoe.js
    claim the winner

--- React-prac-Tic-Tac-Toe/tictactoe.js
+++ React-prac-Tic-Tac-Toe/tictactoe.js
@@ -65,15 +65,22 @@ class Board extends React.Component {
     this.state = {
       squares: Array(9).fill(null),
       xIsNext: true,
+      clickCount: 0,
     }
   }
   //Board controls Square
   handleClick(i){
     const squares = this.state.squares.slice();
+    let cCount = 0;
+    if (calculateWinner(squares)!='None' || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
+    cCount = this.state.clickCount + 1;
     this.setState({
       squares: squares,
-      xIsNext: !this.state.xIsNext,           
+      xIsNext: !this.state.xIsNext,
+      clickCount: cCount,
     });
   }   
   
@@ -90,7 +97,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner != 'None' || this.state.clickCount == 9) {
+      //the clickCount could deal with the bug in the end, which isn't noticed in the official tutorial
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next Player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -129,6 +143,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return 'None';
 }
 
 // ========================================
